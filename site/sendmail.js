aa3da387e4e5a904c0e89830eed6044d3784222d
usermuser <lwrdrstv@gmail.com>
2018-07-30 16:21:31 +0500

    some tests with js

---
+++ site/sendmail.js
@@ -0,0 +1,12 @@
+function get_form_data(){
+	var email = $('#exampleInputEmail1').val();
+	var phone = $('#phone').val();
+	console.log(email, phone);
+
+}
+
+
+$('#submit_button').click(function(){
+	
+	
+})
