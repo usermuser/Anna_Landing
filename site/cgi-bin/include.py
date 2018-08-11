d39c4306aa5effd15f9850ee680351523c7ef33d
usermuser <lwrdrstv@gmail.com>
2018-08-11 23:00:06 +0500

    added include.py

---
+++ site/cgi-bin/include.py
@@ -0,0 +1,35 @@
+
+footer = '''
+<footer class="text-muted bg-dark">
+      <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              	<h4 class="text-white">О Нас</h4>
+              	<p class="text-muted">
+              		По запросу мы предоставим прайс-лист на продукцию предприятия и всю необходимую документацию для создания комфортных условий для совместного сотрудничества. 
+				</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Контакты</h4>
+              <ul class="list-unstyled">
+                <li><a href="https://vk.com/happiness_food174" class="text-white">
+                <i class="fa fa-lg fa-vk" aria-hidden="true"></i>&nbsp happiness_food174</a></li>
+                <li><a href="https://www.instagram.com/happiness_174/" class="text-white"><i class="fa fa-lg fa-instagram" aria-hidden="true"></i>&nbsp happiness_174</a></li>
+                <li><a href="tel:2488815" class="text-white"><i class="fa fa-lg fa-phone" aria-hidden="true"></i>&nbsp Телефон производство 24-888-15</a></li>
+                <li><a href="tel:89658533203" class="text-white"><i class="fa fa-lg fa-phone" aria-hidden="true"></i>&nbsp Мой номер 89658533203</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="sendmail.js"></script>
+    <script>window.jQuery || document.write('<script src="jquery-slim.min.js"><\/script>')</script>
+    <script src="popper.min.js"></script>
+    <script src="bootstrap.js"></script>
+    <script src="holder.min.js"></script>
+    '''
